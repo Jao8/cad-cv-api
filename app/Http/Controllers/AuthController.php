cb56263d02f14b489b79dd96689e502adc73fb1d
Jao8 <joaovictorocosta@gmail.com>
2023-01-12 20:32:56 -0300

    - Codigo inutilizado removido

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,28 +9,6 @@ class AuthController extends Controller
 {
     public function __invoke(Request $request)
     {
-//        $fields = $request->validate([
-//            'email' => 'required|string',
-//            'password' => 'required|string'
-//        ]);
-//
-//        // Check email
-//        $user = User::where('email', $fields['email'])->first();
-//
-//        // Check password
-//        if(!$user || !Hash::check($fields['password'], $user->password)) {
-//            return response([
-//                'message' => 'Bad creds'
-//            ], 401);
-//        }
-//
-//        $token = $user->createToken('myapptoken')->plainTextToken;
-//
-//        $response = [
-//            'user' => $user,
-//            'token' => $token
-//        ];
-
         if(!auth()->attempt($request->only('email', 'password'))) {
             throw new AuthenticationException('error login -> '. $request['email'].'/'.$request['password']);
         }
